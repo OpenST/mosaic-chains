3d0d958659235c0f0b10707fc8da83d169d1ecec
Deepesh Kumar Nath <deepesh@ost.com>
2019-05-07 18:18:24 +0530

    Updated the chain ids of officialIdentifiers

--- src/bin/mosaic-start.ts
+++ src/bin/mosaic-start.ts
@@ -9,14 +9,14 @@ import Logger from '../Logger';
 
 let mosaic = commander
   .version(version)
-  .arguments('<chains...>');
+  .arguments('<chain>');
 
 mosaic = NodeOptions.addCliOptions(mosaic);
 
 mosaic
   .option('-u,--unlock <accounts>', 'a comma separated list of accounts that get unlocked in the node; you must use this together with --password')
   .option('-s,--password <file>', 'the path to the password file on your machine; you must use this together with --unlock')
-  .action((chainIds: string[], options) => {
+  .action((chain: string, options) => {
     let {
       mosaicDir,
       port,
@@ -27,49 +27,28 @@ mosaic
       password,
     } = NodeOptions.parseOptions(options);
 
-    if (chainIds.length > 1) {
-      if (port !== undefined) {
-        Logger.error('cannot use --port with multiple chain ids');
-        process.exit(1);
-      }
-      if (rpcPort !== undefined) {
-        Logger.error('cannot use --rpcPort with multiple chain ids');
-        process.exit(1);
-      }
-      if (websocketPort !== undefined) {
-        Logger.error('cannot use --websocketPort with multiple chain ids');
-        process.exit(1);
-      }
+    const chainId = NodeFactory.getChainId(chain);
+    if (port === undefined) {
+      port = NodeOptions.getPortForChainId(chainId);
+    }
+    if (rpcPort === undefined) {
+      rpcPort = NodeOptions.getRpcPortForChainId(chainId);
+    }
+    if (websocketPort === undefined) {
+      websocketPort = NodeOptions.getWsPortForChainId(chainId);
     }
 
-    for (const chainId of chainIds) {
-      const chainIdNumber = NodeFactory.getChainId(chainId);
-      if (port === undefined) {
-        port = NodeOptions.getPortForChainId(chainIdNumber);
-      }
-      if (rpcPort === undefined) {
-        rpcPort = NodeOptions.getRpcPortForChainId(chainIdNumber);
-      }
-      if (websocketPort === undefined) {
-        websocketPort = NodeOptions.getWsPortForChainId(chainIdNumber);
-      }
+    const node: Node = NodeFactory.create({
+      chainId: chain,
+      mosaicDir,
+      port,
+      rpcPort,
+      websocketPort,
+      keepAfterStop,
+      unlock,
+      password,
+    });
 
-      const node: Node = NodeFactory.create({
-        chainId,
-        mosaicDir,
-        port,
-        rpcPort,
-        websocketPort,
-        keepAfterStop,
-        unlock,
-        password,
-      });
-
-      node.start();
-
-      port = undefined;
-      rpcPort = undefined;
-      websocketPort = undefined;
-    }
+    node.start();
   })
   .parse(process.argv);
