486da8280a4ae23e2357f39943a4c4b0b8dd7bb7
abhayks1 <abhay@ost.com>
2019-06-03 16:44:23 +0530

    eslint fixes

--- src/NewChain/ChainVerifier.ts
+++ src/NewChain/ChainVerifier.ts
@@ -1,13 +1,13 @@
-import Web3 = require('web3');
-import { ContractInteract, Contracts as MosaicContracts, AbiBinProvider } from '@openst/mosaic.js';
+import { Contracts as MosaicContracts, AbiBinProvider } from '@openst/mosaic.js';
 
-import MosaicConfig, {ContractAddresses} from "../Config/MosaicConfig";
+import MosaicConfig, { ContractAddresses } from '../Config/MosaicConfig';
+
+import Web3 = require('web3');
 
 /**
  * Chain verifier does verification of newly created auxiliary chains.
  */
 export default class ChainVerifier {
-
   private originWeb3: Web3;
 
   private auxiliaryWeb3: Web3;
@@ -27,14 +27,14 @@ export default class ChainVerifier {
    * @param {string} auxiliaryWebSocket auxiliary chain web3 endpoint.
    * @param {string} chainId auxiliary chain id.
    */
-  constructor(
+  public constructor(
     private originWebSocket: string,
     private auxiliaryWebSocket: string,
     private chainId: string,
   ) {
     this.originWeb3 = new Web3(originWebSocket);
     this.auxiliaryWeb3 = new Web3(auxiliaryWebSocket);
-    this.mosaicConfig =  MosaicConfig.from(chainId);
+    this.mosaicConfig = MosaicConfig.from(chainId);
     this.mosaicContractInstance = new MosaicContracts(this.originWeb3, this.auxiliaryWeb3);
     this.contractAddresses = this.mosaicConfig.auxiliaryChains[this.chainId].contractAddresses;
     this.abiBinProvider = new AbiBinProvider();
@@ -63,35 +63,35 @@ export default class ChainVerifier {
     const deployedGatewayBin = await this.originWeb3.eth.getCode(
       this.contractAddresses.origin.ostEIP20GatewayAddress,
     );
-    if (deployedGatewayBin != this.abiBinProvider.getBIN("EIP20Gateway")) {
+    if (deployedGatewayBin !== this.abiBinProvider.getBIN('EIP20Gateway')) {
       throw new Error('ContractsBin: Mismatch of Gateway BIN!!!');
     }
 
     const deployedCoGatewayBin = await this.auxiliaryWeb3.eth.getCode(
       this.contractAddresses.auxiliary.ostEIP20CogatewayAddress,
     );
-    if (deployedCoGatewayBin != this.abiBinProvider.getBIN("EIP20CoGateway")) {
+    if (deployedCoGatewayBin !== this.abiBinProvider.getBIN('EIP20CoGateway')) {
       throw new Error('ContractsBin: Mismatch of CoGateway BIN!!!');
     }
 
     const deployedAnchorBin = await this.originWeb3.eth.getCode(
       this.contractAddresses.origin.anchorAddress,
     );
-    if (deployedAnchorBin != this.abiBinProvider.getBIN("Anchor")) {
+    if (deployedAnchorBin !== this.abiBinProvider.getBIN('Anchor')) {
       throw new Error('ContractsBin: Mismatch of Anchor BIN!!!');
     }
 
     const deployedOrganizationBin = await this.originWeb3.eth.getCode(
       this.contractAddresses.origin.ostGatewayOrganizationAddress,
     );
-    if (deployedOrganizationBin != this.abiBinProvider.getBIN("Organization")) {
+    if (deployedOrganizationBin !== this.abiBinProvider.getBIN('Organization')) {
       throw new Error('ContractsBin: Mismatch of Organization BIN!!!');
     }
 
     const deployedOstPrimeBin = await this.auxiliaryWeb3.eth.getCode(
       this.contractAddresses.auxiliary.ostPrimeAddress,
     );
-    if (deployedOstPrimeBin != this.abiBinProvider.getBIN("OSTPrime")) {
+    if (deployedOstPrimeBin !== this.abiBinProvider.getBIN('OSTPrime')) {
       throw new Error('ContractsBin: Mismatch of OSTPrime BIN!!!');
     }
   }
@@ -107,28 +107,28 @@ export default class ChainVerifier {
     );
 
     const isActivated = await gatewayInstance.methods.activated().call();
-    if (isActivated == false) {
+    if (isActivated !== true) {
       throw new Error('Gateway: It should be activated!!!');
     }
     const valueToken = await gatewayInstance.methods.valueToken().call();
-    if (valueToken != this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
+    if (valueToken !== this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
       throw new Error('Gateway: Invalid valueToken address!!!');
     }
     const baseToken = await gatewayInstance.methods.baseToken().call();
-    if (baseToken != this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
+    if (baseToken !== this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
       throw new Error('Gateway: Invalid baseToken address!!!');
     }
     // Verify organization address
     const organization = await gatewayInstance.methods.organization().call();
-    if (organization != this.contractAddresses.origin.ostGatewayOrganizationAddress) {
+    if (organization !== this.contractAddresses.origin.ostGatewayOrganizationAddress) {
       throw new Error('Gateway: Invalid gateway organization address!!!');
     }
     const remoteGateway = await gatewayInstance.methods.remoteGateway().call();
-    if (remoteGateway != this.contractAddresses.auxiliary.ostEIP20CogatewayAddress) {
+    if (remoteGateway !== this.contractAddresses.auxiliary.ostEIP20CogatewayAddress) {
       throw new Error('Gateway: Invalid CoGateway address!!!');
     }
     const stateRootProvider = await gatewayInstance.methods.stateRootProvider().call();
-    if (stateRootProvider != this.contractAddresses.origin.anchorAddress) {
+    if (stateRootProvider !== this.contractAddresses.origin.anchorAddress) {
       throw new Error('Gateway: Invalid stateRootProvider!!!');
     }
   }
@@ -140,26 +140,26 @@ export default class ChainVerifier {
    */
   private async verifyCoGateway(): Promise<void> {
     const coGatewayInstance = this.mosaicContractInstance.EIP20CoGateway(
-      this.contractAddresses.auxiliary.ostEIP20CogatewayAddress
+      this.contractAddresses.auxiliary.ostEIP20CogatewayAddress,
     );
     const valueToken = coGatewayInstance.methods.valueToken().call();
-    if (valueToken != await this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
+    if (valueToken !== await this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
       throw new Error('CoGateway: Invalid valueToken address!!!');
     }
     const utilityToken = coGatewayInstance.methods.utilityToken().call();
-    if (utilityToken != await this.contractAddresses.auxiliary.ostPrimeAddress) {
+    if (utilityToken !== await this.contractAddresses.auxiliary.ostPrimeAddress) {
       throw new Error('CoGateway: Invalid OSTPrime address!!!');
     }
     const organization = await coGatewayInstance.methods.organization().call();
-    if (organization != this.contractAddresses.auxiliary.ostCoGatewayOrganizationAddress) {
+    if (organization !== this.contractAddresses.auxiliary.ostCoGatewayOrganizationAddress) {
       throw new Error('CoGateway: Invalid organization address!!!');
     }
     const remoteGateway = await coGatewayInstance.methods.remoteGateway().call();
-    if (remoteGateway != this.contractAddresses.origin.ostEIP20GatewayAddress) {
+    if (remoteGateway !== this.contractAddresses.origin.ostEIP20GatewayAddress) {
       throw new Error('CoGateway: Invalid remoteGateway address!!!');
     }
     const stateRootProvider = await coGatewayInstance.methods.stateRootProvider().call();
-    if (stateRootProvider != this.contractAddresses.auxiliary.anchorAddress) {
+    if (stateRootProvider !== this.contractAddresses.auxiliary.anchorAddress) {
       throw new Error('CoGateway: Invalid stateRootProvider!!!');
     }
   }
@@ -171,18 +171,18 @@ export default class ChainVerifier {
    */
   private async verifyOriginAnchor(): Promise<void> {
     const anchorInstance = this.mosaicContractInstance.OriginAnchor(
-      this.contractAddresses.origin.anchorAddress
+      this.contractAddresses.origin.anchorAddress,
     );
     const coAnchor = await anchorInstance.methods.coAnchor().call();
-    if (coAnchor != this.contractAddresses.auxiliary.anchorAddress) {
+    if (coAnchor !== this.contractAddresses.auxiliary.anchorAddress) {
       throw new Error('OriginAnchor: Invalid coAnchor address!!!');
     }
     const organization = await anchorInstance.methods.organization().call();
-    if (organization != this.contractAddresses.origin.anchorOrganizationAddress) {
+    if (organization !== this.contractAddresses.origin.anchorOrganizationAddress) {
       throw new Error('OriginAnchor: Invalid organization address!!!');
     }
     const remoteChainId = await anchorInstance.methods.remoteChainId().call();
-    if (remoteChainId != this.chainId) {
+    if (remoteChainId !== this.chainId) {
       throw new Error('OriginAnchor: Invalid remoteChainId!!!');
     }
   }
@@ -197,15 +197,15 @@ export default class ChainVerifier {
       this.contractAddresses.origin.anchorAddress,
     );
     const coAnchor = await anchorInstance.methods.coAnchor().call();
-    if (coAnchor != this.contractAddresses.auxiliary.anchorAddress) {
+    if (coAnchor !== this.contractAddresses.auxiliary.anchorAddress) {
       throw new Error('AuxiliaryAnchor: Invalid coAnchor address!!!');
     }
     const organization = await anchorInstance.methods.organization().call();
-    if (organization != this.contractAddresses.auxiliary.anchorOrganizationAddress) {
+    if (organization !== this.contractAddresses.auxiliary.anchorOrganizationAddress) {
       throw new Error('AuxiliaryAnchor: Invalid organization address!!!');
     }
     const remoteChainId = await anchorInstance.methods.remoteChainId().call();
-    if (organization != this.mosaicConfig.originChain.chain) {
+    if (remoteChainId !== this.mosaicConfig.originChain.chain) {
       throw new Error('AuxiliaryAnchor: Invalid remoteChainId!!!');
     }
   }
@@ -220,21 +220,20 @@ export default class ChainVerifier {
       this.contractAddresses.auxiliary.ostPrimeAddress,
     );
     const valueToken = await ostPrimeInstance.methods.valueToken().call();
-    if (valueToken != this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
+    if (valueToken !== this.mosaicConfig.originChain.contractAddresses.simpleTokenAddress) {
       throw new Error('OSTPrime: Invalid OSTPrime address!!!');
     }
     const initialized = await ostPrimeInstance.methods.initialized().call();
-    if (initialized != true) {
+    if (initialized !== true) {
       throw new Error('OSTPrime: Invalid initialized value!!!');
     }
     const organization = await ostPrimeInstance.methods.organization().call();
-    if (organization != this.contractAddresses.auxiliary.ostCoGatewayOrganizationAddress) {
+    if (organization !== this.contractAddresses.auxiliary.ostCoGatewayOrganizationAddress) {
       throw new Error('OSTPrime: Invalid organization address!!!');
     }
     const coGateway = await ostPrimeInstance.methods.coGateway().call();
-    if (coGateway != this.contractAddresses.auxiliary.ostEIP20CogatewayAddress) {
+    if (coGateway !== this.contractAddresses.auxiliary.ostEIP20CogatewayAddress) {
       throw new Error('OSTPrime: Invalid coGateway address!!!');
     }
   }
-
 }
