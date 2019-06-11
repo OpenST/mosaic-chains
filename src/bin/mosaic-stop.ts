5b362b1b1a96f63560a9e360175a5875e3b72d7a
Puneet Khushwani <puneet.khushwani@ost.com>
2019-06-11 12:47:06 +0530

    in mosaic stop - added code to stop graph related docker containers

--- src/bin/mosaic-stop.ts
+++ src/bin/mosaic-stop.ts
@@ -5,6 +5,8 @@ import { version } from '../../package.json';
 import NodeFactory from '../Node/NodeFactory';
 import NodeDescription from '../Node/NodeDescription';
 import Node from '../Node/Node';
+import GraphDescription from '../Graph/GraphDescription';
+import Graph from '../Graph/Graph';
 
 mosaic
   .version(version)
@@ -12,8 +14,10 @@ mosaic
   .action((chains: string[]) => {
     for (const chain of chains) {
       const node: Node = NodeFactory.create(new NodeDescription(chain));
-
       node.stop();
+      const graph: Graph = new Graph(new GraphDescription(chain));
+      graph.stop();
     }
+
   })
   .parse(process.argv);
