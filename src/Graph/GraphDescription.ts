cabd46251b5ed6ab6a88e8031625467d1c5b9b66
Puneet Khushwani <puneet.khushwani@ost.com>
2019-06-11 13:04:26 +0530

    added GraphDescription class

---
+++ src/Graph/GraphDescription.ts
@@ -0,0 +1,23 @@
+/**
+ * Describes the properties of a graph.
+ */
+export default class GraphDescription {
+
+  public mosaicDir: string = '~/.mosaic';
+
+  public nodeRpcPort: number = 8545;
+
+  public rpcPort: number = 8000;
+
+  public websocketPort: number = 8001;
+
+  public rpcAdminPort: number = 8020;
+
+  public ipfsPort: number = 5001;
+
+  public postgresPort: number = 5432;
+
+  public keepAfterStop: boolean = false;
+
+  constructor(readonly chain: string) { }
+}
