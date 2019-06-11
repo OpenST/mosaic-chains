5c899d16b9ac8a7eb92ee4686887cbae225ffe85
Sarvesh Jain <sarvesh@ost.com>
2019-06-11 12:55:56 +0530

    Fixed liter issues and only keeping required abis for now

--- tools/abi_downloader/index.ts
+++ tools/abi_downloader/index.ts
@@ -1,36 +1,40 @@
-import * as path from "path";
-import * as fs from "fs-extra";
+import * as path from 'path';
+import * as fs from 'fs-extra';
 
-const mosaicContract = require('@openst/mosaic-contracts');
+import Logger from '../../src/Logger';
+import Directory from '../../src/Directory';
 
-import Logger from "../../src/Logger";
-import Directory from "../../src/Directory";
+import mosaicContract = require('@openst/mosaic-contracts');
 
 const args = process.argv.slice(2);
 const version = args[0];
-const abi_folder = './abi';
+const abiFolder = './abi';
+const allowedABIs = [
+  'Anchor',
+  'OSTComposer',
+  'EIP20Gateway',
+  'EIP20CoGateway',
+];
+
+function writeToFile(name, abi, contractVersion): void {
+  const directory = path.join(
+    Directory.projectRoot,
+    abiFolder,
+    contractVersion,
+  );
+  fs.ensureDirSync(directory);
+  const filePath = path.join(
+    directory,
+    `${name}.json`,
+  );
+  Logger.info(`storing ${name} abi to path ${filePath} `);
+
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(abi, null, '    '),
+  );
+}
 
 const contracts = Object.keys(mosaicContract);
-contracts.map(contract =>
-    writeToFile(contract, mosaicContract[contract].abi, version)
-);
-
-function writeToFile(name, abi, version) {
-
-    const directory = path.join(
-        Directory.projectRoot,
-        abi_folder,
-        version
-    );
-    fs.ensureDirSync(directory);
-    const filePath = path.join(
-        directory,
-        `${name}.json`,
-    );
-    Logger.info(`storing ${name} abi to path ${filePath} `);
-
-    fs.writeFileSync(
-        filePath,
-        JSON.stringify(abi, null, '    '),
-    );
-}
+contracts.filter(c => allowedABIs.indexOf(c) !== -1)
+  .map(contract => writeToFile(contract, mosaicContract[contract].abi, version));
