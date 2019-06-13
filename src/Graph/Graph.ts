584a50b66f840c9a691cf31d21cb075020df9682
Puneet Khushwani <puneet.khushwani@ost.com>
2019-06-13 19:32:19 +0530

    remove unused getters from Graph.ts

--- src/Graph/Graph.ts
+++ src/Graph/Graph.ts
@@ -11,31 +11,31 @@ import Directory from '../Directory';
 export default class Graph {
 
   /** The chain identifier identifies the chain this node should run. For example ropsten or 200. */
-  protected chain: string;
+  private readonly chain: string;
 
   /** The dir in which data is stored. */
-  protected mosaicDir: string;
+  private readonly mosaicDir: string;
 
-  /** Docker has published Ethereum RPC port at this port on the host. */
-  protected ethereumRpcPort: number;
+  /** Docker has published Ethereum WS port at this port on the host. */
+  private readonly ethereumRpcPort: number;
 
   /** Docker will publish this RPC port on the host. */
-  protected rpcPort: number;
+  private readonly rpcPort: number;
 
   /** Docker will publish this websocket port on the host. */
-  protected websocketPort: number;
+  private readonly websocketPort: number;
 
   /** Docker will publish this admin JSON-RPC port on the host. */
-  protected rpcAdminPort: number;
+  private readonly rpcAdminPort: number;
 
-  /** Docker will publish this IPFS port on the host. */
-  protected ipfsPort: number;
+  /** Docker will publish this IPFs port on the host. */
+  private readonly ipfsPort: number;
 
   /** Docker will publish this Postgres port on the host. */
-  protected postgresPort: number;
+  private readonly postgresPort: number;
 
   /** The name of the docker container which runs graph node. */
-  protected containerName: string;
+  private readonly containerName: string;
 
   /**
    * the prefix used in network & container names.
@@ -64,34 +64,6 @@ export default class Graph {
     this.postgresPort = graphDescription.postgresPort;
 
     this.containerName = `${Graph.prefix}${this.chain}`;
-  }
-
-  public getChain(): string {
-    return this.chain;
-  }
-
-  public getRpcPort(): number {
-    return this.rpcPort;
-  }
-
-  public getWebsocketPort(): number {
-    return this.websocketPort;
-  }
-
-  public getRpcAdminPort(): number {
-    return this.rpcAdminPort;
-  }
-
-  public getIpfsPort(): number {
-    return this.ipfsPort;
-  }
-
-  public getPostgresPort(): number {
-    return this.postgresPort;
-  }
-
-  public getContainerName(): string {
-    return this.containerName;
   }
 
   /**
