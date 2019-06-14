1424cdd3c210d01ef518380b5e8c259e18ed04b7
Puneet Khushwani <puneet.khushwani@ost.com>
2019-06-14 18:37:15 +0530

    changed let -> const and fixed EOF issues.

--- src/Config/PublishMosaicConfig.ts
+++ src/Config/PublishMosaicConfig.ts
@@ -14,12 +14,12 @@ export default class PublishMosaicConfig {
     const publishMosaicConfigDir = Directory.getPublishMosaicConfigDir;
     const publishedFileNames: Array<string> = fs.readdirSync(publishMosaicConfigDir);
     const publishedFileNamesMap = {};
-    for (let publishedFileName of publishedFileNames) {
+    for (const publishedFileName of publishedFileNames) {
       publishedFileNamesMap[publishedFileName] = 1;
     }
     const projectMosaicConfigDir = Directory.getProjectMosaicConfigDir;
     const toBePublishedFileNames: Array<string> = fs.readdirSync(projectMosaicConfigDir);
-    for (let toBePublishedFileName of toBePublishedFileNames) {
+    for (const toBePublishedFileName of toBePublishedFileNames) {
       if (!publishedFileNamesMap.hasOwnProperty(toBePublishedFileName)) {
         fs.copySync(
           path.join(projectMosaicConfigDir, toBePublishedFileName),
