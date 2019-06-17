8b530883ad11b1c29f8d79f4ac822bc6db0e3c45
Puneet Khushwani <puneet.khushwani@ost.com>
2019-06-17 11:16:54 +0530

    test cases affecting publish mosaic config modified.

--- package.sh
+++ package.sh
@@ -7,4 +7,4 @@ npm ci
 cp -r ./utility_chains ./lib/
 cp -r ./mosaic_configs ./lib/
 cp ./src/Graph/docker-compose.yml ./lib/src/Graph/docker-compose.yml
-
+cp ./src/Config/MosaicConfig.schema.json ./lib/src/Config/MosaicConfig.schema.json
