fa36e1bc1a83826eaa47847a8d1f17439c21f1f6
Sarvesh Jain <sarvesh@ost.com>
2019-06-19 13:01:37 +0530

    Added check for valid version

--- tools/abi_downloader/run.sh
+++ tools/abi_downloader/run.sh
@@ -5,7 +5,14 @@ default="the default value"
 read -p "Enter mosaic contracts version [default=$default_version] " version
 : ${version:=${default_version}}
 
-mosaic_contract_package=${package}$"@"${version}
-echo ${mosaic_contract_package}
-npm install ${mosaic_contract_package} --no-save
-ts-node tools/abi_downloader/index.ts ${version}
+version_count=$(grep -o '\.' <<<"$version" | grep -c .)
+if ((version_count ==1)) || ((version_count ==2))
+then
+    mosaic_contract_package=${package}$"@"${version}
+    echo ${mosaic_contract_package}
+    npm install ${mosaic_contract_package} --no-save
+    ts-node tools/abi_downloader/index.ts ${version}
+else
+    echo "Enter version in format MAJOR.MINOR or MAJOR.MINOR.PATCH"
+    exit
+fi
