308f20f10ce1c23cd0df670ae2e8db51b3311e72
abhayks1 <abhay@ost.com>
2019-10-10 20:28:31 +0530

    update package.sh MosaicConfig path

--- package.sh
+++ package.sh
@@ -8,4 +8,4 @@ cp -r ./chains ./lib/
 cp -r ./graph ./lib/
 cp -r ./abi ./lib/
 cp ./src/Graph/docker-compose.yml ./lib/src/Graph/docker-compose.yml
-cp ./src/Config/MosaicConfig.schema.json ./lib/src/Config/MosaicConfig.schema.json
+cp ./src/Config/MosaicConfig/MosaicConfig.schema.json ./lib/src/Config/MosaicConfig.schema.json
