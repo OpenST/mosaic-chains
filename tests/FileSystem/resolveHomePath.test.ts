c5f1878acd78eaca82dbffd35c834c213dceafcc
abhayks1 <abhay@ost.com>
2019-10-15 16:59:17 +0530

    Tests added for FileSystem.resolveHomePath

--- tests/FileSystem/resolveHomePath.test.ts
+++ tests/FileSystem/resolveHomePath.test.ts
@@ -1,7 +1,28 @@
 import 'mocha';
-
+import FileSystem from "../../src/FileSystem ";
+import { assert } from 'chai';
 
 describe('FileSystem.resolveHomeDirectory()', () => {
 
+  it('Should resolve home path', async () => {
+    const unresolvedHomePath = '~/.mosaic/goerli/mosaic.json';
+    const filePath = FileSystem.resolveHomePath(unresolvedHomePath);
+    const expectedFilePath = `${process.env.HOME}/.mosaic/goerli/mosaic.json`;
+    assert.strictEqual(
+      filePath,
+      expectedFilePath,
+      'Error resolving home directory',
+    )
+  });
 
+  it('Should return correct path if path does not have tilde', async () => {
+    const unresolvedHomePath = '/user/.mosaic/goerli/mosaic.json';
+    const filePath = FileSystem.resolveHomePath(unresolvedHomePath);
+    const expectedFilePath = `/user/.mosaic/goerli/mosaic.json`;
+    assert.strictEqual(
+      filePath,
+      expectedFilePath,
+      'Error resolving home directory',
+    )
+  });
 });
