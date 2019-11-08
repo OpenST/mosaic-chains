a2326df6e00ac3d0069664cabe0da457ec0f20b8
Puneet Khushwani <puneet.khushwani@ost.com>
2019-11-08 12:01:49 +0530

    broke smoke tests into logical pieces

--- tests/smoke.sh
+++ tests/smoke.sh
@@ -5,6 +5,35 @@
 # It asserts that the command generally works.
 #
 
+chain=$1
+
+# 1406 config
+GRAPH_ADMIN_RPC_1406=9426
+GRAPH_IPFS_1406=6407
+OST_COGATEWAY_ADDRESS_1406=0x02cffaa1e06c28021fff6b36d9e418a97b3de2fc
+
+# 1407 config
+GRAPH_ADMIN_RPC_1407=9427
+GRAPH_IPFS_1407=6408
+OST_COGATEWAY_ADDRESS_1407=0xf690624171fe06d02d2f4250bff17fe3b682ebd1
+
+# ropsten config
+GRAPH_ADMIN_RPC_ROPSTEN=8023
+GRAPH_IPFS_ROPSTEN=5004
+GRAPH_WS_PORT_ROPSTEN=60003
+OST_GATEWAY_ADDRESS_ROPSTEN_1406=0x04df90efbedf393361cdf498234af818da14f562
+OST_GATEWAY_ADDRESS_ROPSTEN_1407=0x31c8870c76390c5eb0d425799b5bd214a2600438
+
+# Dev chain config
+GRAPH_ADMIN_RPC_DEV_ORIGIN=9535
+GRAPH_IPFS_DEV_ORIGIN=6516
+GRAPH_WS_PORT_DEV_ORIGIN=61515
+WETH_GATEWAY_ADDRESS_DEV_ORIGIN=0xaE02C7b1C324A8D94A564bC8d713Df89eae441fe
+WETH_CO_GATEWAY_ADDRESS_DEV_ORIGIN=0xc6fF898ceBf631eFb58eEc7187E4c1f70AE8d943
+DEV_AUXILIARY_CHAIN_ID=1000
+GRAPH_ADMIN_RPC_DEV_AUXILIARY=9020
+GRAPH_IPFS_DEV_AUXILIARY=6001
+
 # Prints an info string to stdout.
 function info {
     echo "INFO: $1"
@@ -52,7 +81,7 @@ function stop_nodes {
 # $3 chain {origin, auxiliary}
 # $4 graph admin rpc port
 # $5 graph IPFS port
-function  deploy_subgraph {
+function  deploy_subgraph_mosaic_config {
     info "Deploying $3 subraph."
     try_silent "./mosaic subgraph $1 $2 $3 http://localhost:$4 http://localhost:$5"
 }
@@ -68,7 +97,6 @@ function  deploy_subgraph_gateway_config {
     info "Deploying $3 subraph."
     try_silent "./mosaic subgraph $1 $2 $3 http://localhost:$4 http://localhost:$5 --gateway-config ~/.mosaic/$1/$2/gateway-$6/gateway-config.json"
 }
-
 
 # Tries a command without output. Errors if the command does not execute successfully.
 function try_silent {
@@ -134,91 +162,80 @@ function rpc_auxiliary_sub_graph_try {
     try_silent "./node_modules/.bin/ts-node tests/Graph/SubGraphDeployment/auxiliary-verifier.ts 6$1 $2" "Auxiliary sub graph was expected to be deployed, but wasn't."
 }
 
+function test_ropsten {
+    start_origin_node ropsten geth
+    sleep 25
+    grep_try ropsten geth
+    rpc_node_try "0003" # Given like this as it is used for the port in `rpc_node_try`.
+    deploy_subgraph_mosaic_config ropsten 1406 origin $GRAPH_ADMIN_RPC_ROPSTEN $GRAPH_IPFS_ROPSTEN
+    sleep 25
+    rpc_origin_sub_graph_try $GRAPH_WS_PORT_ROPSTEN $OST_GATEWAY_ADDRESS_ROPSTEN_1406
+    deploy_subgraph_mosaic_config ropsten 1407 origin $GRAPH_ADMIN_RPC_ROPSTEN $GRAPH_IPFS_ROPSTEN
+    sleep 25
+    rpc_origin_sub_graph_try $GRAPH_WS_PORT_ROPSTEN $OST_GATEWAY_ADDRESS_ROPSTEN_1407
+    stop_node ropsten
+    grep_fail ropsten geth
+    start_origin_node ropsten parity
+    grep_try ropsten parity
+    stop_node ropsten
+    grep_fail ropsten parity
+}
+
+function test_1406 {
+    start_auxiliary_node 1406
+    sleep 25
+    grep_try 1406 geth
+    rpc_node_try 1406
+    deploy_subgraph_mosaic_config ropsten 1406 auxiliary $GRAPH_ADMIN_RPC_1406 $GRAPH_IPFS_1406
+    sleep 25
+    rpc_auxiliary_sub_graph_try 1406 $OST_COGATEWAY_ADDRESS_1406
+    stop_node 1406
+    grep_fail 1406 geth
+}
+
+function test_1407 {
+    start_auxiliary_node 1407
+    sleep 25
+    grep_try 1407 geth
+    rpc_node_try 1407
+    deploy_subgraph_mosaic_config ropsten 1407 auxiliary $GRAPH_ADMIN_RPC_1407 $GRAPH_IPFS_1407
+    sleep 25
+    rpc_auxiliary_sub_graph_try 1407 $OST_COGATEWAY_ADDRESS_1407
+    stop_node 1407
+    grep_fail 1407 geth
+}
+
+function test_dev_origin {
+  start_origin_node dev-origin geth
+  sleep 25
+  deploy_subgraph_gateway_config dev-origin $DEV_AUXILIARY_CHAIN_ID origin $GRAPH_ADMIN_RPC_DEV_ORIGIN $GRAPH_IPFS_DEV_ORIGIN $WETH_GATEWAY_ADDRESS_DEV_ORIGIN
+  sleep 25
+  rpc_origin_sub_graph_try  $GRAPH_WS_PORT_DEV_ORIGIN $WETH_GATEWAY_ADDRESS_DEV_ORIGIN
+  stop_node dev-origin
+}
+
+function test_dev_auxiliary {
+  start_auxiliary_node dev-auxiliary geth
+  sleep 25
+  deploy_subgraph_gateway_config dev-origin $DEV_AUXILIARY_CHAIN_ID auxiliary $GRAPH_ADMIN_RPC_DEV_AUXILIARY $GRAPH_IPFS_DEV_AUXILIARY $WETH_GATEWAY_ADDRESS_DEV_ORIGIN
+  sleep 25
+  rpc_auxiliary_sub_graph_try $DEV_AUXILIARY_CHAIN_ID $WETH_CO_GATEWAY_ADDRESS_DEV_ORIGIN
+  stop_node dev-auxiliary
+}
+
 # Making sure the mosaic command exists (we are in the right directory).
 try_silent "ls mosaic" "Script must be run from the mosaic chains root directory so that the required node modules are available."
 
-info "Starting node one by one and verifying if all services for them are running."
-# 1406 config
-GRAPH_ADMIN_RPC_1406=9426
-GRAPH_IPFS_1406=6407
-OST_COGATEWAY_ADDRESS_1406=0x02cffaa1e06c28021fff6b36d9e418a97b3de2fc
-
-# 1407 config
-GRAPH_ADMIN_RPC_1407=9427
-GRAPH_IPFS_1407=6408
-OST_COGATEWAY_ADDRESS_1407=0xf690624171fe06d02d2f4250bff17fe3b682ebd1
-
-# ropsten config
-GRAPH_ADMIN_RPC_ROPSTEN=8023
-GRAPH_IPFS_ROPSTEN=5004
-GRAPH_WS_PORT_ROPSTEN=60003
-OST_GATEWAY_ADDRESS_ROPSTEN_1406=0x04df90efbedf393361cdf498234af818da14f562
-OST_GATEWAY_ADDRESS_ROPSTEN_1407=0x31c8870c76390c5eb0d425799b5bd214a2600438
-
-# Dev chain config
-GRAPH_ADMIN_RPC_DEV_ORIGIN=9535
-GRAPH_IPFS_DEV_ORIGIN=6516
-GRAPH_WS_PORT_DEV_ORIGIN=61515
-OST_GATEWAY_ADDRESS_DEV_ORIGIN_WETH=0xaE02C7b1C324A8D94A564bC8d713Df89eae441fe
-OST_CO_GATEWAY_ADDRESS_DEV_ORIGIN_WETH=0xc6fF898ceBf631eFb58eEc7187E4c1f70AE8d943
-
-DEV_AUXILIARY_CHAIN_ID=1000
-GRAPH_ADMIN_RPC_DEV_AUXILIARY=9020
-GRAPH_IPFS_DEV_AUXILIARY=6001
-
-start_origin_node ropsten geth
-sleep 50
-grep_try ropsten geth
-rpc_node_try "0003" # Given like this as it is used for the port in `rpc_node_try`.
-deploy_subgraph ropsten 1406 origin $GRAPH_ADMIN_RPC_ROPSTEN $GRAPH_IPFS_ROPSTEN
-sleep 25
-rpc_origin_sub_graph_try $GRAPH_WS_PORT_ROPSTEN $OST_GATEWAY_ADDRESS_ROPSTEN_1406
-deploy_subgraph ropsten 1407 origin $GRAPH_ADMIN_RPC_ROPSTEN $GRAPH_IPFS_ROPSTEN
-sleep 25
-rpc_origin_sub_graph_try $GRAPH_WS_PORT_ROPSTEN $OST_GATEWAY_ADDRESS_ROPSTEN_1407
-
-start_auxiliary_node 1406
-sleep 50
-grep_try 1406 geth
-rpc_node_try 1406
-deploy_subgraph ropsten 1406 auxiliary $GRAPH_ADMIN_RPC_1406 $GRAPH_IPFS_1406
-sleep 25
-rpc_auxiliary_sub_graph_try 1406 $OST_COGATEWAY_ADDRESS_1406
-
-start_auxiliary_node 1407
-sleep 50
-grep_try 1407 geth
-rpc_node_try 1407
-deploy_subgraph ropsten 1407 auxiliary $GRAPH_ADMIN_RPC_1407 $GRAPH_IPFS_1407
-sleep 25
-rpc_auxiliary_sub_graph_try 1407 $OST_COGATEWAY_ADDRESS_1407
-
-# Stop and start some nodes and make sure they are or are not running.
-stop_node ropsten
-grep_fail ropsten geth
-
-stop_node 1407
-grep_fail 1407 geth
-grep_try 1406 geth
-
-start_auxiliary_node 1407
-grep_try 1407 geth
-grep_try 1406 geth
-grep_fail ropsten geth
-
-start_origin_node ropsten parity
-grep_try ropsten parity
-
-# Deploy subgraph with gateway config
-start_origin_node dev-origin geth
-sleep 50
-start_auxiliary_node dev-auxiliary geth
-sleep 50
-deploy_subgraph_gateway_config dev-origin $DEV_AUXILIARY_CHAIN_ID origin $GRAPH_ADMIN_RPC_DEV_ORIGIN $GRAPH_IPFS_DEV_ORIGIN $OST_GATEWAY_ADDRESS_DEV_ORIGIN_WETH
-sleep 25
-rpc_origin_sub_graph_try  $GRAPH_WS_PORT_DEV_ORIGIN $OST_GATEWAY_ADDRESS_DEV_ORIGIN_WETH
-deploy_subgraph_gateway_config dev-origin $DEV_AUXILIARY_CHAIN_ID auxiliary $GRAPH_ADMIN_RPC_DEV_AUXILIARY $GRAPH_IPFS_DEV_AUXILIARY $OST_GATEWAY_ADDRESS_DEV_ORIGIN_WETH
-sleep 25
-rpc_auxiliary_sub_graph_try $DEV_AUXILIARY_CHAIN_ID $OST_CO_GATEWAY_ADDRESS_DEV_ORIGIN_WETH
-# When done, stop all nodes.
-stop_nodes
+if [ $chain = "ropsten" ]; then
+	test_ropsten
+elif [ $chain = "1406" ]; then
+	test_1406
+elif [ $chain = "1407" ]; then
+	test_1407
+elif [ $chain = "dev-origin" ]; then
+	test_dev_origin
+elif [ $chain = "dev-auxiliary" ]; then
+	test_dev_auxiliary
+else
+	echo "invalid input"
+fi
